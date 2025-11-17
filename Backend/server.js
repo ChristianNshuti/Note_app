9445cb969e59bca7b1b162750cfc03821df12db4
christiannshuti <chrisnshuti943@gmail.com>
2025-11-17 19:30:13 +0200

    Updated cors policy for server.js

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:5174','frontend-gray-two-18.vercel.app'],
+    origin: ['http://localhost:5173','http://localhost:5174','https://frontend-gray-two-18.vercel.app'],
     credentials:true
 }));
 
